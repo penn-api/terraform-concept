9ebc4745772f6fe66d93745a7e17babf4d6d00c1
subh <subh@gmail.com>
2025-07-12 11:50:01 +0530

    data block concept

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-2"
+}
+
+data "aws_security_group" "example" {
+  tags = {
+    env   = "dev"
+  }
+}
+
+
+resource "aws_instance" "this" {
+  ami                     = "ami-01c837d5176a7605d"
+  instance_type           = "t3.nano"
+  vpc_security_group_ids = [data.aws_security_group.example.id]
+
+}
